6dacae979dae0a7bb8a2c6f07f7613eb771f8b8d
Siarhei Mamykin <bymamykin@gmail.com>
2022-07-04 03:18:51 +0400

    o

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
-import HomePage from "../pages/HomePage";
-import LoginPage from "../pages/LoginPage";
-import ErrorPage from "../pages/ErrorPage";
-import PlayVideoPage from "../pages/PlayVideoPage";
-import { useSelector } from "react-redux";
-import { userSelector } from "../redux/slices/userSlice";
-import { paths } from "../constants";
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import ErrorPage from '../pages/ErrorPage';
+import PlayVideoPage from '../pages/PlayVideoPage';
+import { useSelector } from 'react-redux';
+import { userSelector } from '../redux/slices/userSlice';
+import { paths } from '../constants';
 
 const Navigator: React.FC<{
   setWalletAddress(walletAddress: string): void;
 }> = ({ setWalletAddress }) => {
   const [isEthereum, setIsEthereum] = useState<boolean>(false);
-  const [address, setAddress] = useState<string>("");
+  const [address, setAddress] = useState<string>('');
   const currentUser = useSelector(userSelector);
 
   useEffect(() => {
@@ -33,7 +33,7 @@ const Navigator: React.FC<{
   return (
     <Routes>
       <Route
-        path="/"
+        path='/'
         element={
           currentUser.user.length === 0 ? (
             <Navigate replace to={paths.Login} />
@@ -43,17 +43,26 @@ const Navigator: React.FC<{
         }
       />
       <Route
-        path="/login"
+        path='/login'
         element={
           isEthereum ? (
             <LoginPage setAddress={setAddress} />
           ) : (
-            <ErrorPage errorTitle={"You need to get Metamask extension"} />
+            <ErrorPage errorTitle={'You need to get Metamask extension'} />
           )
         }
       />
-      <Route path="*" element={<ErrorPage />} />
-      <Route path="/watch" element={<PlayVideoPage />} />
+      <Route path='*' element={<ErrorPage />} />
+      <Route
+        path='/watch'
+        element={
+          currentUser.user.length === 0 ? (
+            <Navigate replace to={paths.Login} />
+          ) : (
+            <PlayVideoPage />
+          )
+        }
+      />
     </Routes>
   );
 };
